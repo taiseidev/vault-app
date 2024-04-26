835563f06728e98e1507759eb28680a3f0183768
onishi taisei <taisei.developer@gmail.com>
2024-04-26 15:20:15 +0900

    🔧 chore: Add Firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vault_app/debug_screen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
